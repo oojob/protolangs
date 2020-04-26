1199d244e3a5e5ebdd537416cb705d3812f13e95
nirajgeorgian <niraj.kishore@stayology.com>
2020-04-26 09:47:15 +0530

    test #3

--- .githooks/create-changelog.sh
+++ .githooks/create-changelog.sh
@@ -9,14 +9,7 @@
 #
 # Copyright (c) 2020 - oojob
 
-# Check that we want to commit.
+# create a changelog
 
-read -p "Are you sure you want to commit this (y/n)? " answer
-case ${answer:0:1} in
-    y|Y )
-        exit 0 # If yes, success!
-    ;;
-    * )
-        exit 1 # If no, sorry yo.
-    ;;
-esac
+git log --pretty="- %s" > CHANGELOG.md
+exit 0
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 git hooks are used for maintaining codes
 
 ```bash
-git config core.hooksPath .githooks
+git config --local core.hooksPath .githooks
 ```
 
 [Contribution guidelines for this project](CONTRIBUTING.md)
